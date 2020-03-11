b9d1365cdcfbdaf2bfcdecaa7fb6c794e1eacd68
yameste <yameste@yahoo.co.uk>
2020-03-11 10:23:10 +0000

    first commit

--- DateTime/Program.cs
+++ DateTime/Program.cs
@@ -6,7 +6,10 @@ namespace DateTime
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Invoke Constructor
+            // Parameters mean: (year, month, day, hour, minute, second)
+            var date01 = new DateTime(2008, 5, 1, 8, 30, 52);
+            Console.WriteLine(date01);
         }
     }
 }
